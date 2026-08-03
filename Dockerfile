f24c067dffe10bd4d810f10d6d94ab18fe5ca429
Michael Rechkunov <mrechkunov@gmail.com>
2026-08-03 14:32:19 +0300

    infra ready

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# --- Этап 1: Сборка приложения ---
+FROM golang:1.26.5-trixie AS builder
+
+WORKDIR /app
+
+# Копируем файлы зависимостей
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+
+# Копируем исходный код
+COPY . .
+
+# Компилируем бинарник (CGO отключен для статической линковки)
+RUN CGO_ENABLED=0 GOOS=linux go build -o gophprofile ./cmd/server/main.go
+
+# --- Этап 2: Финальный образ ---
+FROM alpine
+
+WORKDIR /root/
+
+# Копируем скомпилированный файл из предыдущего шага
+COPY /migrations/* ./migrations/
+COPY --from=builder /app/gophprofile .
+
+# Указываем порт, который слушает приложение
+#EXPOSE 8080
+
+# Запуск приложения
+CMD ["./gophprofile"]
---
+++ internal/config/config.go
@@ -0,0 +1,88 @@
+package config
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"gophprofile/internal/logger"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+// DB connection
+var DBconn *sql.DB
+
+type Addresses struct {
+	ServerBindAddress string
+	MigrationsPath    string
+	DBConnStr         string
+}
+
+var ConfigAddresses = Addresses{
+	ServerBindAddress: "localhost:8080",
+	MigrationsPath:    "",
+	DBConnStr:         "",
+}
+
+func Init() {
+	ba := flag.String("a", "localhost:8080", "adress to server run")
+	mp := flag.String("m", "file://migrations", "default migration PATH")
+	cs := flag.String("d", "postgres://postgres:secret@postgres:5432/postgres?sslmode=disable", "default DBConnStr")
+	flag.Parse()
+
+	// если переиенные окружения установленны, берем их, иначе берем флаг
+	if serverAddress, isEnvBindSrv := os.LookupEnv("RUN_ADDRESS"); isEnvBindSrv {
+		ConfigAddresses.ServerBindAddress = serverAddress
+	} else {
+		ConfigAddresses.ServerBindAddress = *ba
+	}
+
+	if migrationsPath, isEnvMigrationsPath := os.LookupEnv("MIGRATIONS_PATH"); isEnvMigrationsPath {
+		ConfigAddresses.MigrationsPath = migrationsPath
+	} else {
+		ConfigAddresses.MigrationsPath = *mp
+	}
+	if dbConnStr, isEnvDBConnStr := os.LookupEnv("DATABASE_URI"); isEnvDBConnStr {
+		ConfigAddresses.DBConnStr = dbConnStr
+	} else {
+		ConfigAddresses.DBConnStr = *cs
+	}
+	// create connect to DB and run Up all migrations
+	var err error
+	DBconn, err = NewConnect()
+	if err != nil {
+		logger.Log.Errorln("error while connecting to DB (configure service)", err)
+	}
+	migrations(DBconn)
+}
+func NewConnect() (*sql.DB, error) {
+	db, err := sql.Open("pgx", ConfigAddresses.DBConnStr)
+	if err != nil {
+		logger.Log.Errorln(err)
+	}
+	return db, err
+}
+
+func migrations(DBconn *sql.DB) {
+	m, err := migrate.New(
+		ConfigAddresses.MigrationsPath,
+		ConfigAddresses.DBConnStr)
+	fmt.Println(ConfigAddresses.MigrationsPath)
+	fmt.Println(ConfigAddresses.DBConnStr)
+	if err != nil {
+		logger.Log.Errorln("error initializing migrate:", err)
+	}
+	// Apply all available migrations
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		logger.Log.Errorln("error applying migrations:", err)
+	}
+	logger.Log.Infoln("database migrations applied successfully!")
+	err = DBconn.Ping()
+	if err != nil {
+		logger.Log.Warnln("error while ping DB after migratioans applied", err)
+	}
+}
